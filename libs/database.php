196c788399a6b4c8706040a9307ef51a32deb151
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-05-14 22:54:55 -0500

    cambio en la conexión de db

--- libs/database.php
+++ libs/database.php
@@ -20,7 +20,7 @@ class Database{
     
         try{
             
-            $connection = "mysql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset;
+            $connection = "pgsql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset;
             $options = [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_EMULATE_PREPARES   => false,
