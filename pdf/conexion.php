64ae9d02c7ac5862550f6f9d2add07110f99f840
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-05-16 12:03:33 -0500

    camnbio archivo conexion

--- pdf/conexion.php
+++ pdf/conexion.php
@@ -1,6 +1,6 @@
 <?php
 	
-	$mysqli = new mysqli('localhost', 'root', '', 'salon3');
+	$mysqli = new mysqli('ec2-18-233-32-61.compute-1.amazonaws.com', 'aexfxezwrawarg', '787475b9a8d0a1761af0628a0f4457a8593455032347be5d00a900b78fdf98c9', 'd1bh0aame8tlh9');
 	
 	if($mysqli->connect_error){
 		
