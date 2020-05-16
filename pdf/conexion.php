9a59050087178121258998ae6ea99cf81e8e5644
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-05-16 17:03:27 -0500

    Update conexion.php

--- pdf/conexion.php
+++ pdf/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$usuario=aexfxezwrawarg;
-$pass=787475b9a8d0a1761af0628a0f4457a8593455032347be5d00a900b78fdf98c9;;
-$host=ec2-18-233-32-61.compute-1.amazonaws.com;
-$bd=d1bh0aame8tlh9;
+$usuario='aexfxezwrawarg';
+$pass='787475b9a8d0a1761af0628a0f4457a8593455032347be5d00a900b78fdf98c9';
+$host='ec2-18-233-32-61.compute-1.amazonaws.com';
+$bd='d1bh0aame8tlh9';
 
     function conectar_PostgreSQL( $usuario, $pass, $host, $bd )
     {
