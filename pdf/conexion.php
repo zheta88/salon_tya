7b510cf7fc860392bca3eafeee7c941b20e7f7fd
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-05-16 16:57:06 -0500

    Update conexion.php

--- pdf/conexion.php
+++ pdf/conexion.php
@@ -1,10 +1,16 @@
 <?php
-	
-	$pgsql = new pgsql('ec2-18-233-32-61.compute-1.amazonaws.com', 'aexfxezwrawarg', '787475b9a8d0a1761af0628a0f4457a8593455032347be5d00a900b78fdf98c9', 'd1bh0aame8tlh9');
-	
-	if($pgsql->connect_error){
-		
-		die('Error en la conexion' . $pgsql->connect_error);
-		
-	}
+$usuario=aexfxezwrawarg;
+$pass=787475b9a8d0a1761af0628a0f4457a8593455032347be5d00a900b78fdf98c9;;
+$host=ec2-18-233-32-61.compute-1.amazonaws.com;
+$bd=d1bh0aame8tlh9;
+
+    function conectar_PostgreSQL( $usuario, $pass, $host, $bd )
+    {
+         $conexion = pg_connect( "user=".$usuario." ".
+                                "password=".$pass." ".
+                                "host=".$host." ".
+                                "dbname=".$bd
+                               ) or die( "Error al conectar: ".pg_last_error() );
+        return $conexion;
+    }
 ?>
