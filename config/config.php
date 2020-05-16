90849f002476049fe206d0bfeb14349037999e97
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-05-16 10:38:53 -0500

    agrego nombre db remota

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 define('URL', 'https://salondebellezatya.herokuapp.com/');
 
 define('HOST', 'ec2-18-233-32-61.compute-1.amazonaws.com');
-define('DB', '');
+define('DB', 'd1bh0aame8tlh9');
 define('USER', '');
 define('PASSWORD', '');
 define('CHARSET', '');
