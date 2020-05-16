cb9875b8caf9184c65ee566625b83358280e84b6
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-05-16 17:11:54 -0500

    Update conexion.php

--- pdf/conexion.php
+++ pdf/conexion.php
@@ -1,16 +1,13 @@
 <?php
-$usuario='aexfxezwrawarg';
-$pass='787475b9a8d0a1761af0628a0f4457a8593455032347be5d00a900b78fdf98c9';
-$host='ec2-18-233-32-61.compute-1.amazonaws.com';
-$bd='d1bh0aame8tlh9';
 
-    function conectar_PostgreSQL( $usuario, $pass, $host, $bd )
-    {
-         $conexion = pg_connect( "user=".$usuario." ".
-                                "password=".$pass." ".
-                                "host=".$host." ".
-                                "dbname=".$bd
-                               ) or die( "Error al conectar: ".pg_last_error() );
-        return $conexion;
-    }
-?>
+$pass = "787475b9a8d0a1761af0628a0f4457a8593455032347be5d00a900b78fdf98c9";
+$user = "aexfxezwrawarg";
+$bd = "d1bh0aame8tlh9";
+$rutaServidor = "ec2-18-233-32-61.compute-1.amazonaws.com";
+$puerto = "5432";
+try {
+    $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$bd", $user, $pass);
+    $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo "Ocurrió un error con la base de datos: " . $e->getMessage();
+}
