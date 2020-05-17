5682b3d76d919d7378ab9fceb6e58866e43878bb
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-05-16 19:52:11 -0500

    quito charset

--- config/config.php
+++ config/config.php
@@ -6,6 +6,5 @@ define('HOST', 'ec2-18-233-32-61.compute-1.amazonaws.com');
 define('DB', 'd1bh0aame8tlh9');
 define('USER', 'aexfxezwrawarg');
 define('PASSWORD', '787475b9a8d0a1761af0628a0f4457a8593455032347be5d00a900b78fdf98c9');
-define('PUERTO', '5432');
 
 ?>
--- libs/database.php
+++ libs/database.php
@@ -6,21 +6,21 @@ class Database{
     private $db;
     private $user;
     private $password;
-    private $puerto;
+  
 
     public function __construct(){
         $this->host     = constant('HOST');
         $this->db       = constant('DB');
         $this->user     = constant('USER');
         $this->password = constant('PASSWORD');
-        $this->puerto  = constant('PUERTO');
+;
     }
 
     function connect(){
     
         try{
             
-            $connection = "pgsql:host=" . $this->host . ";dbname=" . $this->db . ";puerto=" . $this->puerto;
+            $connection = "pgsql:host=" . $this->host . ";dbname=" . $this->db;
             $options = [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_EMULATE_PREPARES   => false,
