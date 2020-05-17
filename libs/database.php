8ecf1da521eec76d54b9720d0c889643e132ac79
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-05-16 19:42:13 -0500

    agrego puerto al config

--- libs/database.php
+++ libs/database.php
@@ -6,21 +6,21 @@ class Database{
     private $db;
     private $user;
     private $password;
-    private $charset;
+    private $puerto;
 
     public function __construct(){
         $this->host     = constant('HOST');
         $this->db       = constant('DB');
         $this->user     = constant('USER');
         $this->password = constant('PASSWORD');
-        $this->charset  = constant('CHARSET');
+        $this->puerto  = constant('PUERTO');
     }
 
     function connect(){
     
         try{
             
-            $connection = "pgsql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset;
+            $connection = "pgsql:host=" . $this->host . ";dbname=" . $this->db . ";puerto=" . $this->puerto;
             $options = [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_EMULATE_PREPARES   => false,
