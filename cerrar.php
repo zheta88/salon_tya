b1ceff824a7aa2135ec22b7d80870472342fe540
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-05-20 15:53:22 -0500

    cerrar sesion sin tiempo

--- cerrar.php
+++ cerrar.php
@@ -1,46 +1,5 @@
 <?php
-//opcion 1 ( funciona)---------------
-//session_start ();
-//session_destroy ();
-//header ("location: login");
-//------------------------------------------
-
-//opcion 2 ( probando)----------------------
-
-session_start();
-
-// Máxima duración de sesión activa en hora
-define( 'MAX_SESSION_TIEMPO', 3 * 1 );
-
-// Controla cuando se ha creado y cuando tiempo ha recorrido 
-if ( isset( $_SESSION[ 'ULTIMA_ACTIVIDAD' ] ) && 
-     ( time() - $_SESSION[ 'ULTIMA_ACTIVIDAD' ] > MAX_SESSION_TIEMPO ) ) {
-
-    // Si ha pasado el tiempo sobre el limite destruye la session
-    destruir_session();
-}
-
-$_SESSION[ 'ULTIMA_ACTIVIDAD' ] = time();
-
-// Función para destruir y resetear los parámetros de sesión
-function destruir_session() {
-
-    $_SESSION = array();
-    if ( ini_get( 'session.use_cookies' ) ) {
-        $params = session_get_cookie_params();
-        setcookie(
-            session_name(),
-            '',
-            time() - MAX_SESSION_TIEMPO,
-            $params[ 'path' ],
-            $params[ 'domain' ],
-            $params[ 'secure' ],
-            $params[ 'httponly' ] );
-    }
-
-    @session_destroy();
-    header ("location: login");
-}
-
+session_start ();
+session_destroy ();
+header ("location: login");
 ?>
-
