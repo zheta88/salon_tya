e5f8409a5e6b9a942031a3dd5c95eee86dc16a7c
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-06-03 14:22:04 -0500

    logo y nombre

--- pdf/indexrol.php
+++ pdf/indexrol.php
@@ -1,12 +1,13 @@
 <?php
-	include 'plantilla.php';
-    include_once '../libs/database.php';
+	
+    include 'plantilla.php';
+	include_once '../libs/database.php';
 	include '../config/config.php';
     $conexion = new Database();
-    $consultarol=$_POST ['ROL_idROL'];
+    $consultarol=$_POST ['rol'];
 	
-	$query = "SELECT * FROM personas where ROL_idROL='".$consultarol."'";
-	$resultado = $mysqli->query($query);
+	$query = "SELECT * FROM Personas where  ROL_idROL='".$consultarol."'";
+	$resultado = $conexion->connect()->query($query);
 	
 	$pdf = new PDF();
 	$pdf->AliasNbPages();
@@ -14,7 +15,7 @@
 	
 	$pdf->SetFillColor(2,157,116);
 	$pdf->SetFont('Arial','B',10);
-	$pdf->Cell(35,6,'NO. DE DOCUMENTO',1,0,'C',1);
+	$pdf->Cell(35,6,'nro_documento',1,0,'C',1);
 	$pdf->Cell(35,6,'NOMBRE',1,0,'C',1);
 	$pdf->Cell(35,6,'APELLIDO',1,0,'C',1);
 	// $pdf->Cell(35,6,'CELULAR',1,0,'C',1);
@@ -29,13 +30,13 @@
 	{
 		
 		$pdf->Cell(35,6,utf8_decode($row['nro_documento']),1,0,'C');
-		$pdf->Cell(35,6,utf8_decode($row['Nombre']),1,0,'C');
-		$pdf->Cell(35,6,utf8_decode($row['Apellidos']),1,0,'C');
+		$pdf->Cell(35,6,utf8_decode($row['nombre']),1,0,'C');
+		$pdf->Cell(35,6,utf8_decode($row['apellidos']),1,0,'C');
 		// $pdf->Cell(35,6,$row['Celular'],1,1,'C',0);
 		// $pdf->Cell(35,6,$row['Direccion'],1,1,'C',0);
-		 $pdf->Cell(40,6,$row['Correo'],1,0,'C');
+		 $pdf->Cell(40,6,$row['correo'],1,0,'C');
 		// $pdf->Cell(35,6,$row['Contrasena'],1,1,'C',0);
-		 $pdf->Cell(35,6,$row['ROL_idROL'],1,1,'C');
+		 $pdf->Cell(35,6,$row['rol_idrol'],1,1,'C');
 	}
 	$pdf->Output();
 ?>
