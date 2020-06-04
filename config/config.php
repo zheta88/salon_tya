55d00a37dd87495361297977351394f74844c8b0
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-06-04 10:07:03 -0500

    prueba

--- config/config.php
+++ config/config.php
@@ -1,10 +1,12 @@
 <?php
 
-define('URL', 'http://localhost/salon2-master/');
+//define('URL', 'http://localhost/salon2-master/');
 
-define('HOST', 'localhost');
-define('DB', 'salon3');
-define('USER', 'postgres');
-define('PASSWORD', '123');
+//define('HOST', 'localhost');
+//define('DB', 'salon3');
+//define('USER', 'postgres');
+//define('PASSWORD', '123');
+///////////////////////////////////////////////////////
+
 
 ?>
