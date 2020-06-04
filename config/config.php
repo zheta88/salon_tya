a262d42ac4bb95b24dffe63d53170536346ca4cb
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-06-04 10:08:11 -0500

    prueba config

--- config/config.php
+++ config/config.php
@@ -8,5 +8,10 @@
 //define('PASSWORD', '123');
 ///////////////////////////////////////////////////////
 
+define('URL', 'https://salondebellezatya.herokuapp.com/');
 
+define('HOST', 'ec2-3-91-139-25.compute-1.amazonaws.com');
+define('DB', 'd8d9f1m6i3ge1m');
+define('USER', 'uvionmdvurwyen');
+define('PASSWORD', '27da61d46f2846bf65f741f01cdcd48bad396e3f5a639bdd758f8982f2d48d3d');
 ?>
