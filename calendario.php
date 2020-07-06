417710669e7e2bdd28ca4c365b08708c826cdb24
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-07-06 18:52:39 -0500

    boostrap calendario

--- calendario.php
+++ calendario.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
+<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie-edge">
 	<title>Calendario Web</title>
-	
-	<link rel='stylesheet'  href='public/css/fullcalendar.min.css'/>
-	<script  src='public/js/jquery.min.js'></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script  src='public/js/jquery.min.js'></script>
 	<script  src='public/js/moment.min.js'></script>
+    <!--full calendar-->
+	<link rel='stylesheet'  href='public/css/fullcalendar.min.css'/>
 	<script  src='public/js/fullcalendar.min.js'></script>
-	<script  src='public/js/es.js'></script>
-
+    <script  src='public/js/es.js'></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
 <body>
 
