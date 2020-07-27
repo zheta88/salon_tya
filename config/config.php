9476225dd8ca243d13501ebdac52c4f4a97e91b0
zheta88 <52215922+zheta88@users.noreply.github.com>
2020-07-27 11:09:51 -0500

    m

--- config/config.php
+++ config/config.php
@@ -1,17 +1,17 @@
 <?php
 
-define('URL', 'http://localhost/salon2-master/');
+// define('URL', 'http://localhost/salon2-master/');
 
-define('HOST', 'localhost');
-define('DB', 'salon3');
-define('USER', 'postgres');
-define('PASSWORD', '123');
+// define('HOST', 'localhost');
+// define('DB', 'salon3');
+// define('USER', 'postgres');
+// define('PASSWORD', '123');
 /////////////////////////////////////////////////////
 
-// define('URL', 'https://salondebellezatya.herokuapp.com/');
+define('URL', 'https://salondebellezatya.herokuapp.com/');
 
-// define('HOST', 'ec2-3-91-139-25.compute-1.amazonaws.com');
-// define('DB', 'd8d9f1m6i3ge1m');
-// define('USER', 'uvionmdvurwyen');
-// define('PASSWORD', '27da61d46f2846bf65f741f01cdcd48bad396e3f5a639bdd758f8982f2d48d3d');
+define('HOST', 'ec2-3-91-139-25.compute-1.amazonaws.com');
+define('DB', 'd8d9f1m6i3ge1m');
+define('USER', 'uvionmdvurwyen');
+define('PASSWORD', '27da61d46f2846bf65f741f01cdcd48bad396e3f5a639bdd758f8982f2d48d3d');
 ?>
